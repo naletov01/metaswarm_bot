ff90049b4589e05566d00eb462aaac9db2495b09
naletov01 <67475301+naletov01@users.noreply.github.com>
2025-08-02 12:15:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import logging
 from fastapi import FastAPI, Request, HTTPException
-from telegram import Update
+from telegram import Update, BotCommand
 
 import config
 import handlers
@@ -14,10 +14,13 @@ from telegram.ext import (
     CallbackContext,
     CallbackQueryHandler
 )
+import replicate
 
 from menu import CB_MAIN
 from config import bot, WEBHOOK_PATH
-from handlers import start, image_upload_handler, text_handler, menu_callback, on_check_sub
+from handlers import (
+    start, image_upload_handler, text_handler, menu_callback, 
+    on_check_sub, choose_model, profile, info, partner)
 
 # подтягиваем Bot и путь вебхука из config
 bot = config.bot
@@ -30,10 +33,22 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 dp = Dispatcher(bot=bot, update_queue=None, use_context=True)
 
+config.bot.set_my_commands([
+    BotCommand("start",        "🏠 Главное меню"),
+    BotCommand("choose_model", "🎞 Генерация видео"),
+    BotCommand("profile",      "👤 Профиль"),
+    BotCommand("info",         "ℹ️ О генеративных моделях"),
+    BotCommand("partner",      "🤑 Партнёрская программа"),
+])
+
 # ——— Регистрация хендлеров ———
+dp.add_handler(CommandHandler("start",        start))
+dp.add_handler(CommandHandler("choose_model", choose_model))
+dp.add_handler(CommandHandler("profile",      profile))
+dp.add_handler(CommandHandler("info",         info))
+dp.add_handler(CommandHandler("partner",      partner))
 dp.add_handler(CallbackQueryHandler(menu_callback, pattern="^menu:"))
 dp.add_handler(CallbackQueryHandler(on_check_sub, pattern="^check_sub$"))
-dp.add_handler(CommandHandler("start", start))
 dp.add_handler(MessageHandler(Filters.photo | (Filters.document & Filters.document.mime_type("image/")), image_upload_handler))
 dp.add_handler(MessageHandler(Filters.text & ~Filters.command, text_handler))
 
