37ce94d89eadde087467af2af5758083bcfaa3a2
naletov01 <67475301+naletov01@users.noreply.github.com>
2025-08-02 15:06:09 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -9,6 +9,8 @@ from telegram import Update
 import config
 from menu import render_menu, MENUS
 from menu import render_menu, CB_MAIN
+from .config import bot
+from .handlers import check_subscription, send_subscribe_prompt
 
 
 def _keep_upload_action(bot, chat_id, stop_event):
