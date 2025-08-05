24e60207a201ddfae065049ef5ac7a988a4bee61
naletov01 <67475301+naletov01@users.noreply.github.com>
2025-08-05 18:21:41 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,4 +1,7 @@
-
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config import DATABASE_URL
 
 Base = declarative_base()
 SessionLocal = sessionmaker(autoflush=False, autocommit=False)
