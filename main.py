0b721d2d85ba46e5977e3d629b82243f5f2d195f
naletov01 <67475301+naletov01@users.noreply.github.com>
2025-08-05 18:20:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,19 +42,6 @@ bot = config.bot
 WEBHOOK_PATH = config.WEBHOOK_PATH
 DATABASE_URL = config.DATABASE_URL
 
-Base = declarative_base()
-SessionLocal = sessionmaker(autoflush=False, autocommit=False)
-
-if DATABASE_URL.startswith("sqlite://"):
-    engine = create_engine(
-        DATABASE_URL,
-        connect_args={"check_same_thread": False},
-        echo=True # временно тру, поменять на False
-    )
-else:
-    engine = create_engine(DATABASE_URL, echo=False)
-SessionLocal.configure(bind=engine)
-
 class User(Base):
     __tablename__ = 'users'
     user_id         = Column(Integer, primary_key=True, index=True)
