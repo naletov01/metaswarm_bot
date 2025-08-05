3de2aa6b27887f2fccff04714398ea92d88f844a
naletov01 <67475301+naletov01@users.noreply.github.com>
2025-08-05 15:39:38 +0300

    Create test_db.py

---
+++ test_db.py
@@ -0,0 +1,17 @@
+# test_db.py
+from main import SessionLocal, User
+
+db = SessionLocal()
+# 1) добавляем тестового юзера
+u = User(user_id=424242, credits=5)
+db.add(u)
+db.commit()
+
+# 2) читаем его обратно
+u2 = db.query(User).filter_by(user_id=424242).first()
+print("Loaded user:", u2.user_id, "credits=", u2.credits)
+
+# 3) чистим за собой (удаляем запись)
+db.delete(u2)
+db.commit()
+db.close()
