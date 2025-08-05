7a4af6df7fba1d88dac053231c3c68493e019a35
naletov01 <67475301+naletov01@users.noreply.github.com>
2025-08-05 18:51:55 +0300

    Create db_utils.py

---
+++ db_utils.py
@@ -0,0 +1,13 @@
+from db import SessionLocal
+from models import User
+import logging
+
+logger = logging.getLogger(__name__)
+
+# — Получить или создать профиль
+def get_user(db: Session, user_id: int) -> User:
+    user = db.query(User).filter(User.user_id == user_id).first()
+    if not user:
+        user = User(user_id=user_id)
+        db.add(user); db.commit(); db.refresh(user)
+    return user
