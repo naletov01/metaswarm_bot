3a43fbabfbbc24bcc8aca6d83c5462c4868eb578
naletov01 <67475301+naletov01@users.noreply.github.com>
2025-08-05 18:20:23 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,14 @@
 
+
+Base = declarative_base()
+SessionLocal = sessionmaker(autoflush=False, autocommit=False)
+
+if DATABASE_URL.startswith("sqlite://"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False},
+        echo=True # временно тру, поменять на False
+    )
+else:
+    engine = create_engine(DATABASE_URL, echo=False)
+SessionLocal.configure(bind=engine)
