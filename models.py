a741d1f75c3be559b51e44906ee88c3a462dbfa7
naletov01 <67475301+naletov01@users.noreply.github.com>
2025-08-05 18:23:54 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,15 @@
+from sqlalchemy import Column, Integer, Boolean, DateTime, String
+from datetime import datetime
+from db import Base
 
+class User(Base):
+    __tablename__ = 'users'
+    user_id         = Column(Integer, primary_key=True, index=True)
+    credits         = Column(Integer, default=0, nullable=False)
+    premium         = Column(Boolean, default=False, nullable=False)
+    subscription_type = Column(String, nullable=True)
+    premium_until   = Column(DateTime, nullable=True)
+    invited_count   = Column(Integer, default=0, nullable=False)
+    bonus_credits   = Column(Integer, default=0, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
