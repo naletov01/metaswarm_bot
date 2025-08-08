8656642d07b28dd52cd8b705191a3493ea1dfc20
naletov01 <67475301+naletov01@users.noreply.github.com>
2025-08-08 20:02:10 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,14 +5,14 @@ from db import Base
 class User(Base):
     __tablename__ = 'users'
     
-    user_id         = Column(Integer, primary_key=True, index=True)
+    user_id         = Column(BigInteger, primary_key=True, index=True)
     credits         = Column(Integer, default=0, nullable=False)
     premium         = Column(Boolean, default=False, nullable=False)
     subscription_type = Column(String, nullable=True)
     premium_until   = Column(DateTime, nullable=True)
     invited_count   = Column(Integer, default=0, nullable=False)
     bonus_credits   = Column(Integer, default=0, nullable=False)
-    referrer_id = Column(Integer, nullable=True)
+    referrer_id = Column(BigInteger, nullable=True)
     
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
