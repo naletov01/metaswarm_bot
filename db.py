e867cddc1a65ebe52174e68946f25121e02f5242
naletov01 <67475301+naletov01@users.noreply.github.com>
2025-08-08 13:35:05 +0300

    Update db.py

--- db.py
+++ db.py
@@ -12,7 +12,7 @@ if DATABASE_URL.startswith("sqlite://"):
     engine = create_engine(
         DATABASE_URL,
         connect_args={"check_same_thread": False},
-        echo=True # временно тру, поменять на False
+        echo=False # временно тру, поменять на False
     )
 else:
     engine = create_engine(DATABASE_URL, echo=False)
